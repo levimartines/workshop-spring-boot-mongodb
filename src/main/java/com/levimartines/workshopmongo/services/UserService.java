f0d117ed06e46803e555abe2bca02a6449c5b263
Levi Martines <levi.mferreira@gmail.com>
2019-05-10 16:43:30 -0300

    Deleção de usuário com DELETE

--- src/main/java/com/levimartines/workshopmongo/services/UserService.java
+++ src/main/java/com/levimartines/workshopmongo/services/UserService.java
@@ -30,7 +30,12 @@ public class UserService {
 		return repository.insert(obj);
 	}
 	
+	public void delete (String id) {
+		findById(id);
+		repository.deleteById(id);
+	}
+	
 	public User fromDTO(UserDTO objDto) {
-		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());	
 	}
 }
