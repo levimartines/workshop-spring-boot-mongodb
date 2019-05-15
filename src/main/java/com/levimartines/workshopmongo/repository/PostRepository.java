74e435f7e8c07f73961ed34d56be0a0ddbf7f944
Levi Martines <levi.mferreira@gmail.com>
2019-05-15 12:32:45 -0300

    Criando entity Post com User aninhado

---
+++ src/main/java/com/levimartines/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,10 @@
+package com.levimartines.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.levimartines.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+}
