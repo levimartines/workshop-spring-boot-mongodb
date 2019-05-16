28d5330c056a61e2679b1986edea1b1398fe9e82
Levi Martines <levi.mferreira@gmail.com>
2019-05-15 22:15:11 -0300

    Obtendo um post por id

---
+++ src/main/java/com/levimartines/workshopmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.levimartines.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.levimartines.workshopmongo.domain.Post;
+import com.levimartines.workshopmongo.repository.PostRepository;
+import com.levimartines.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repository;
+
+	public Post findById(String id) {
+		Optional<Post> obj = repository.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
