4f18c0750aead7b67472dcb84378199ffdaf5edc
levimartines <levi.mferreira@gmail.com>
2019-11-18 22:59:49 -0300

    implementacao de testes

--- src/main/java/com/levimartines/workshopmongo/dto/UserDTO.java
+++ src/main/java/com/levimartines/workshopmongo/dto/UserDTO.java
@@ -1,46 +1,30 @@
 package com.levimartines.workshopmongo.dto;
 
-import java.io.Serializable;
-
 import com.levimartines.workshopmongo.domain.User;
+import java.io.Serializable;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class UserDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private String id;
 	private String name;
 	private String email;
-	
-	public UserDTO() {	
+
+	public UserDTO() {
 	}
-	
+
 	public UserDTO (User obj) {
 		id = obj.getId();
 		name = obj.getName();
 		email = obj.getEmail();
 	}
 
-	public String getId() {
-		return id;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
+	public UserDTO(String name, String email) {
 		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
 		this.email = email;
 	}
 }
--- src/test/java/com/levimartines/workshopmongo/WorkshopmongoApplicationTests.java
+++ src/test/java/com/levimartines/workshopmongo/WorkshopmongoApplicationTests.java
@@ -1,16 +1,56 @@
 package com.levimartines.workshopmongo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.levimartines.workshopmongo.dto.UserDTO;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment= WebEnvironment.RANDOM_PORT)
 public class WorkshopmongoApplicationTests {
 
+	@LocalServerPort
+	int randomServerPort;
+
+	private ObjectMapper MAPPER = new ObjectMapper();
+
+ 	@Test
+	public void insertAuthorReturn201() throws URISyntaxException {
+		UserDTO user = new UserDTO("Levi Martines","levi@gmail.com");
+		RestTemplate restTemplate = new RestTemplate();
+		String baseUrl = "http://localhost:"+randomServerPort+"/users/";
+		URI uri = new URI(baseUrl);
+
+		ResponseEntity<?> result = restTemplate.postForEntity(uri, user, String.class);
+		//Verify request succeed
+		Assert.assertEquals(201, result.getStatusCodeValue());
+	}
+
 	@Test
-	public void contextLoads() {
+	public void getUsuariosReturn201() throws URISyntaxException, IOException {
+		RestTemplate restTemplate = new RestTemplate();
+		String baseUrl = "http://localhost:" + randomServerPort + "/users";
+		ResponseEntity<String> response = restTemplate.getForEntity(new URI(baseUrl), String.class);
+
+		List<UserDTO> users = MAPPER.readValue(response.getBody(),
+			MAPPER.getTypeFactory().constructCollectionType(List.class, UserDTO.class));
+		//Verify request succeed
+		Assert.assertEquals(200, response.getStatusCodeValue());
+		Assertions.assertThat(users.size()).isGreaterThan(0);
 	}
 
 }
